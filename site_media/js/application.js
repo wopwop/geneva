04d1b82db5e22bbf0387dd6cf26bb1d1a04009fc
aniss <aniss@aniss-Vostro-1015.(none)>
2011-10-19 03:15:05 +0100

    home page done

---
+++ site_media/js/application.js
@@ -0,0 +1,51 @@
+$(function(){
+   
+/*   var LatLng = new google.maps.LatLng(49.47216, -123.76307);
+   
+   var MapOptions = {
+      center: LatLng,
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+   };
+   
+   var Mymap = new google.maps.Map(document.getElementById("map"), MapOptions);
+   
+   
+   var marker = new google.maps.Marker({
+      map: Mymap,
+      draggable: true,
+      position: new google.maps.LatLng(49.47216, -123.76307),
+      visible: true,
+      id: 99
+   });
+   
+   var boxText = document.createElement("div");
+   boxText.style.cssText = "border: 1px solid black; margin-top: 8px; background: yellow; padding: 5px;";
+   boxText.innerHTML = "City Hall, Sechelt<br><a href='javascript:alert(3)'>British Columbia</a><br>Canada";
+
+   var boxOptions = {
+      content: boxText,
+      disableAutoPan: false,
+      pixelOffset: new google.maps.Size(-140, 0),
+      closeBoxMargin: "10px 2px 2px 2px",
+      boxStyle: { 
+                  background: "url('tipbox.gif') no-repeat"
+                  ,opacity: 0.75
+                  ,width: "280px"
+                 },
+      closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
+      infoBoxClearance: new google.maps.Size(1, 1),
+      isHidden: false,
+      pane: "floatPane",
+      boxId: 99
+   };
+
+   var ib = new InfoBox;
+   google.maps.event.addListener(marker, "click", function(event){
+      alert(this.get("id"))
+   });
+*/
+
+
+   
+});
